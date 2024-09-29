c54a78f5022f57c2fca8c34e85e604a2194526b4
michaelanyigba <kwabenahlegacy18@gmail.com>
2024-09-29 16:37:06 -0700

    some changes

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://liv-backend-2.onrender.com", {
                 query: {
                     userId: authUser._id
                 }
@@ -37,3 +37,42 @@ export const SocketContextProvider = ({ children }) => {
 
     return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>
 }
+// import { createContext, useContext, useEffect, useState } from "react";
+// import {useAuthContext} from "./AuthContext"
+// import {io} from "socket.io-client"
+
+// const SocketContext = createContext();
+
+// export const useSocketContext = () => {
+//     return useContext(SocketContext)
+// }
+
+// export const SocketContextProvider = ({ children }) => {
+//     const [socket, setSocket] = useState(null);
+//     const [onlineUsers, setOnlineUsers] = useState([])
+//     const { authUser } = useAuthContext()
+
+//     useEffect(() => {
+//         if (authUser) {
+//             const socket = io("http://localhost:5000", {
+//                 query: {
+//                     userId: authUser._id
+//                 }
+//             });
+
+//             setSocket(socket);
+//             socket.on("getOnlineUsers", (users) => {
+//                 setOnlineUsers(users);
+//             })
+//             return () => socket.close();
+//         } else {
+//             if (socket) {
+//                 socket.close();
+//                 setSocket(null);
+//             }
+//         }
+//     }, [authUser])
+
+
+//     return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>
+// }
