67c5b617d0ad695985a1b7914ebf39d4238ee295
michaelanyigba <kwabenahlegacy18@gmail.com>
2024-09-29 11:17:24 -0700

    deploying

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // This should point to the folder where your build is placed
+  },
   plugins: [react()],
   server: {
     port: 3000,
