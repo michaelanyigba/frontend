34554cdc6c6cf1bd17e9a17dbda60dc82f7493f9
michaelanyigba <kwabenahlegacy18@gmail.com>
2024-09-29 16:13:16 -0700

    changes in vite

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api":{
-        target: "https://liv-backend-2.onrender.com"
+        target: "http://localhost:5000"
       }
     }
   },
