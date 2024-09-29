335e5dc6fa16260671c6a61ccf7f9323c30f527c
michaelanyigba <kwabenahlegacy18@gmail.com>
2024-09-29 10:55:38 -0700

    changing the url

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,25 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api":{
-        target: "http://localhost:5000"
+        target: "https://liv-frontend.onrender.com"
       }
     }
   },
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       "/api":{
+//         target: "http://localhost:5000"
+//       }
+//     }
+//   },
+// })
+
