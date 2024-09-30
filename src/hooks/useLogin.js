e4f2c004a284e4dfacc3234f88e512d6d0bd14f3
michaelanyigba <kwabenahlegacy18@gmail.com>
2024-09-30 00:12:02 -0700

    remove credentials

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -17,9 +17,7 @@ const useLogin = () => {
             const res = await fetch("https://liv-backend-2.onrender.com/api/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ username, password }),
-                credentials: 'include'
-            })
+                body: JSON.stringify({ username, password })            })
             const data = await res.json()
             if (data.error) {
                 throw new Error(data.error)
